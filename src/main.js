b1070561d913b6f716e04bcb14a2c7b73c9a7211
saburali <saburali.me@gmail.com>
2023-07-25 10:26:08 +0600

    Install bootstrap / bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@ import App from './App.vue'
 
 import router from "@/routes";
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
+
 import {createHead} from "@vueuse/head";
 const head = createHead()
 
-createApp(App).use(head).use(router).mount('#app')
+createApp(App).use(head).use(router, bootstrap).mount('#app')
