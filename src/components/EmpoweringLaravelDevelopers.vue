afd9016fe97727955b19969f43d2147609d40a70
saburali <saburali.me@gmail.com>
2023-07-26 16:27:05 +0600

    Empowering Laravel Developers

---
+++ src/components/EmpoweringLaravelDevelopers.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="gradient-bg p-4 rounded">
+    <img class="img-fluid" src="../assets/images/verify-mark.svg" alt="Verify Mark" />
+    <h4 class="fw-bold mt-3"> {{ title }} </h4>
+    <p class="mb-0 fs-16 text-secondary min-height-118 overFollow-3dots overFollow-5line"> {{ desc }} </p>
+    <router-link class="btn text-decoration-none fs-16 fw-600 text-dark py-2 btn-icon-animation" :to="{destination}"> {{ btn_name }} <img class="icon" src="../assets/images/arrow-up-right.svg" alt="Arrow" /> </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EmpoweringLaravelDevelopers",
+  props: {
+    title: String,
+    desc: String,
+    btn_name: String,
+    destination: String,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -17,6 +17,32 @@
         free_tutorials_member="5"
         free_tutorials_title="Free Tutorials"
     />
+
+    <!-- Empowering Laravel Developers -->
+    <div class="py-5 section-light-deep bg-image">
+      <div class="container py-0 py-lg-5">
+        <div class="row">
+          <div class="col-12 col-lg-6">
+            <h2 class="fw-bold fs-36 m-0" id="lineBreak-text"> Empowering Laravel Developers </h2>
+          </div>
+          <div class="col-12 col-lg-6 d-flex align-items-end">
+            <p class="m-0 text-secondary"> Join us at LaravelPoint and unlock your full potential as a Laravel developer.  </p>
+          </div>
+        </div>
+
+        <!-- Cards -->
+        <div class="row mt-5">
+          <div class="col-12 col-sm-6 col-md-4 mb-4 mb-lg-0" v-for="item in laravelDeveloperData" :key="item.id">
+            <EmpoweringLaravelDevelopers
+              :title="item.title"
+              :desc="item.desc"
+              :btn_name="item.btn_name"
+              :destination="item.destination"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -24,10 +50,11 @@
 import { reactive } from "vue";
 import { useHead } from "@vueuse/head";
 import HeroComponents from "@/components/Hero";
+import EmpoweringLaravelDevelopers from "@/components/EmpoweringLaravelDevelopers";
 
 export default {
   name: "HomePage",
-  components: {HeroComponents},
+  components: {EmpoweringLaravelDevelopers, HeroComponents},
   setup() {
     const siteData = reactive({
       title: `Laravel Point`,
@@ -54,6 +81,33 @@ export default {
       };
     });
   },
+  data() {
+    return {
+      laravelDeveloperData: [
+        {
+          id: 1,
+          title: "Solve Challenges",
+          desc: "Lorem ipsum dolor sit amet consectetur. Posuere sapien et pellentesque morbi sed viverra leo id. Rhoncus mattis nibh orci commodo leo.",
+          btn_name: "Get Started",
+          destination: "/contact",
+        },
+        {
+          id: 2,
+          title: "Start Practicing",
+          desc: "Lorem ipsum dolor sit amet consectetur. Posuere sapien et pellentesque morbi sed viverra leo id. Rhoncus mattis nibh orci commodo leo.",
+          btn_name: "Get Started",
+          destination: "/challenge",
+        },
+        {
+          id: 3,
+          title: "Watch Tutorials",
+          desc: "Lorem ipsum dolor sit amet consectetur. Posuere sapien et pellentesque morbi sed viverra leo id. Rhoncus mattis nibh orci commodo leo.",
+          btn_name: "Get Started",
+          destination: "/home",
+        },
+      ]
+    }
+  }
 };
 </script>
 
