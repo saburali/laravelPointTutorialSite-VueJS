0cb266ef930fcc833379bfffac197f4a0fb5c92f
saburali <saburali.me@gmail.com>
2023-07-29 22:45:13 +0600

    Small Hero - Add

--- src/pages/Course.vue
+++ src/pages/Course.vue
@@ -1,10 +1,20 @@
 <template>
-
+  <!-- Hero Section -->
+  <SmallHero
+      title="Be A Laravel Maestro"
+      route="/home"
+      route_name="Home"
+      current_route="/course"
+      current_route_name="Course"
+  />
 </template>
 
 <script>
+import SmallHero from "@/components/SmallHero";
+
 export default {
   name: "CoursePage",
+  components: {SmallHero},
 }
 </script>
 
